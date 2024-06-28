41fd38b853b176dc87f84d7c666a10394bccf751
Emine Shaban <emineshaban@icloud.com>
2024-06-28 15:22:29 +0300

    .

--- js/index.js
+++ js/index.js
@@ -60,3 +60,78 @@ for (i = 0; i < li.length; i++) {
     });
 }
 
+
+
+
+
+
+
+
+
+//pagination
+
+let digit = document.getElementsByClassName("digit");
+const onChange = (e) => {
+  if (e.target.textContent == "1") {
+    console.log(e.target.parentElement.children);
+    for (
+      let i = 0;
+      i < e.target.parentElement.parentElement.children.length;
+      i++
+    ) {
+      if (
+        i >= 3 &&
+        i < e.target.parentElement.parentElement.children.length - 1
+      ) {
+        e.target.parentElement.parentElement.children[i].style.display = "none";
+      }
+    }
+    e.target.parentElement.parentElement.children[3].style.display = "block";
+    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+      e.target.parentElement.children[i].classList = "digit";
+    }
+    e.target.classList = "digit active";
+  } else if (e.target.textContent == "2") {
+    console.log(e.target.parentElement.children);
+    for (
+      let i = 0;
+      i < e.target.parentElement.parentElement.children.length;
+      i++
+    ) {
+      if (
+        i >= 3 &&
+        i < e.target.parentElement.parentElement.children.length - 1
+      ) {
+        e.target.parentElement.parentElement.children[i].style.display = "none";
+      }
+    }
+    e.target.parentElement.parentElement.children[4].style.display = "block";
+    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+      e.target.parentElement.children[i].classList = "digit";
+    }
+    e.target.classList = "digit active";
+  } else if (e.target.textContent == "3") {
+    console.log(e.target.parentElement.children);
+    for (
+      let i = 0;
+      i < e.target.parentElement.parentElement.children.length;
+      i++
+    ) {
+      if (
+        i >= 3 &&
+        i < e.target.parentElement.parentElement.children.length - 1
+      ) {
+        e.target.parentElement.parentElement.children[i].style.display = "none";
+      }
+    }
+    e.target.parentElement.parentElement.children[5].style.display = "block";
+    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+      e.target.parentElement.children[i].classList = "digit";
+    }
+    e.target.classList = "digit active";
+  }
+};
+
+for (let index = 0; index < digit.length; index++) {
+  digit[index].addEventListener("click", onChange);
+}
